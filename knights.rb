e4511d5581b592f73fed5c2f8f30b0e5105d87d1
NaimiNafis <126528516+NaimiNafis@users.noreply.github.com>
2024-04-24 17:20:37 +0900

    Refactor all lines into Class KnightMoves Set visited hash to = { start => true } Shorten #is_valid? if statement Private #is_valid? and #build_path

--- knights.rb
+++ knights.rb
@@ -1,54 +1,55 @@
-require 'pry-byebug'
+# frozen_string_literal: true
 
-def knight_moves(start, goal)
-    return 'Please type the correct x and y coordinate' if !start.is_a?(Array) || !goal.is_a?(Array) 
+class KnightMoves
+  def initialize
+    @chess_board = Array.new(8) { Array.new(8) }
+    @moves = [[1, 2], [2, 1], [2, -1], [1, -2],
+              [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+  end
 
-    chess_board = Array.new(8){Array.new(8)}
-    moves = [[1, 2], [2, 1], [2, -1], [1, -2], 
-             [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+  def find_path(start, goal)
+    return 'Please type the correct x and y coordinate' unless start.is_a?(Array) && goal.is_a?(Array)
 
     queue = [start]
-    visited = {}
-    node_before = {start => nil}
-    
-    while !queue.empty?
-        current = queue.shift # [0, 0]
+    visited = { start => true }
+    node_before = { start => nil }
 
-        return build_path(current, node_before, start) if current == goal
+    until queue.empty?
+      current = queue.shift
 
-        moves.each do |x, y|
-            next_node = [x + current[0] , y + current[1]]
+      return build_path(current, node_before, start) if current == goal
 
-            if is_valid?(next_node, chess_board) && !visited.include?(next_node)
-                visited[next_node] = true
-                node_before[next_node] = current 
-                queue.push(next_node)
-            end
-        end
+      @moves.each do |x, y|
+        next_node = [x + current[0], y + current[1]]
+
+        next unless is_valid?(next_node) && !visited[next_node]
+
+        visited[next_node] = true
+        node_before[next_node] = current
+        queue.push(next_node)
+      end
     end
+  end
 
-end
+  private
 
-def is_valid?(position, chess_board)
+  def is_valid?(position)
     x, y = position
-    if x.between?(0, chess_board.length) && y.between?(0, chess_board.length)
-        return true
-    else 
-        return false
-    end
-end
+    x.between?(0, 7) && y.between?(0, 7)
+  end
 
-def build_path(node, node_before, start)
+  def build_path(node, node_before, start)
     path = []
-    
+
     while node != start
-        path.unshift(node)
-        node = node_before[node]
+      path.unshift(node)
+      node = node_before[node]
     end
     path.unshift(start)
     puts "You made it in #{path.size - 1} moves!  Here's your path:"
-    path.each_with_index { |move, index| puts "#{index}: #{move}" }
+    path.each_with_index { |move, index| puts "#{index}: #{move.inspect}" }
+  end
 end
 
-# binding.pry
-knight_moves([0,0], [6,6])
+knight = KnightMoves.new
+knight.find_path([0, 0], [6, 6])
