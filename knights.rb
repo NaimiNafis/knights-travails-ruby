7de4f3c38a01e7faf857558aea1556e5b6723361
NaimiNafis <126528516+NaimiNafis@users.noreply.github.com>
2024-04-24 16:09:17 +0900

    Add #build_path to traverse from start to end

--- knights.rb
+++ knights.rb
@@ -1,16 +1,9 @@
-=begin
-
-1. Set Chess Board dimension = Array[7][7]
-2. Set knight moves = [[1, 2], [2, 1], [2, -1], [1, -2], 
-                        [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
-3. The idea is to use BFS, queue, to keep children 
-
 chess_board = Array.new(8){Array.new(8)}
 moves = [[1, 2], [2, 1], [2, -1], [1, -2], 
         [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
 
 def knight_moves(start, goal)
-    return 'Please type the correct x and y coordinate' if start.is_a?(Array) || goal.is_a?(Array) 
+    return 'Please type the correct x and y coordinate' if !start.is_a?(Array) || !goal.is_a?(Array) 
 
     queue = [start]
     visited = {}
@@ -19,19 +12,21 @@ def knight_moves(start, goal)
     while queue.empty?
         current = queue.shift # [0, 0]
 
+        return build_path(current, node_before) if current == goal
+
         moves.each do |x, y|
             next_node = [x + current[0] , y + current[1]]
 
             if next_node.is_valid?(next_node) && !visited.include?(next_node) # true, false 
                 visited.add(next_node)
                 node_before[next_node] = current
-                
-
+                queue.push(next_node)
+            end
         end
     end
+
 end
 
-#check if the position is valid
 def is_valid?(position, chess_board)
     x, y = position
     if x.between?(0, chess_board.length) && y.between?(0, chess_board.length)
@@ -40,6 +35,31 @@ def is_valid?(position, chess_board)
         return false
     end
 end
+
+def build_path(node, node_before)
+    path = []
+    
+    while node_before
+        path.unshift(node)
+        node = node_before[node]
+    end
+    path
+end
+
+p knight_moves([0,0], [3,3])
+
+=begin
+
+arr = []
+hash = { [0,0] => [2,3], [2,3] => [1,2], [1,2] => [4,6]}
+
+ar.push(hash[[0,0]]) #[2,3]
+
+
+1. Set Chess Board dimension = Array[7][7]
+2. Set knight moves = [[1, 2], [2, 1], [2, -1], [1, -2], 
+                        [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+3. The idea is to use BFS, queue, to keep children 
 
 for each moves, add them to start position, 
     check if it is over chess_board dimension
@@ -55,5 +75,7 @@ dequeue the start_position and put it in [temp_moves], run each children
 
 if each children = goal
     then return temp_move
+
+
 
 =end
